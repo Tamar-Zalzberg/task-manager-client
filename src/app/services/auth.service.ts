bf63f6205323d923e070fc23f2f7f1c9b3aed7fb
Tamar Zalzberg <t0527114699@gmail.com>
2026-02-01 20:37:18 +0200

    Fix users list by adding getUsers function and updating URL

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // הוספנו HttpHeaders
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs'; // הוספנו Observable
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,22 @@ export class AuthService {
   private tokenKey = 'token';
 
   constructor(private http: HttpClient) { }
+
+  // פונקציה עזר לקבלת הטוקן לכותרות (הכרחי כדי לגשת לרשימת משתמשים)
+  private getHeaders() {
+    const token = localStorage.getItem(this.tokenKey);
+    return {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      })
+    };
+  }
+
+  // --- הפונקציה שחסרה לך וגורמת לחץ לא לעבוד! ---
+  getUsers(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/users`, this.getHeaders());
+  }
+
   register(user: any) {
     return this.http.post(`${this.apiUrl}/register`, user).pipe(
       tap((response: any) => {
