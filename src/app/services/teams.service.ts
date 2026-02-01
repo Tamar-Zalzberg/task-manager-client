1d52e2aa1db161ce8b938c2ef046a9b56754d158
Tamar Zalzberg <t0527114699@gmail.com>
2026-02-01 19:30:19 +0200

    Update API URL from localhost to Render production

--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TeamsService {
-  private apiUrl = 'http://localhost:3000/api/teams';
 
+private apiUrl = 'https://wolf-server-dzci.onrender.com/api/teams';
   constructor(private http: HttpClient, private authService: AuthService) { }
 
   private getHeaders() {
